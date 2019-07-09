d3eb85b352c7b6f5b690d636845f5bc1f835fade
Britney Ting <britneyting@fb.com>
2019-07-09 10:24:27 -0700

    Initial Commit

---
+++ Fitness App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fitness App
+//
+//  Created by britneyting on 7/9/19.
+//  Copyright © 2019 britneyting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fitness App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fitness App
+//
+//  Created by britneyting on 7/9/19.
+//  Copyright © 2019 britneyting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
