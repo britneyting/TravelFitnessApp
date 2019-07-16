8ac2c8df8e8a03870341d752c435300305343bf2
Britney Ting <britneyting@fb.com>
2019-07-16 16:34:18 -0700

    user can login and signup for an account

---
+++ Fitness App/View Controllers/SignupViewController.h
@@ -0,0 +1,22 @@
+//
+//  SignupViewController.h
+//  Fitness App
+//
+//  Created by britneyting on 7/16/19.
+//  Copyright © 2019 britneyting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignupViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *usernameField;
+@property (strong, nonatomic) IBOutlet UITextField *emailField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordField;
+@property (strong, nonatomic) IBOutlet UIButton *signupButton;
+
+@end
+
+NS_ASSUME_NONNULL_END
