04d3be02a0bf6b3c5e9645d07e8f65643426d193
Daniela Guajardo <danyguajiba@fb.com>
2019-07-17 09:51:26 -0700

    fixed the login segue, it now changes screens, added the logout feature and also made sure that if the app was closed without the user logging out the user stayed authenticated

--- Fitness App/View Controllers/ProfileViewController.m
+++ Fitness App/View Controllers/ProfileViewController.m
@@ -9,6 +9,10 @@
 #import <UIKit/UIKit.h>
 #import "ProfileViewController.h"
 #import <MapKit/MapKit.h>
+#import "Parse/Parse.h"
+#import "AppDelegate.h"
+#import "LoginViewController.h"
+
 
 @interface ProfileViewController ()
 
@@ -19,6 +23,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+- (IBAction)didTapLogout:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        if(PFUser.currentUser == nil) {
+            AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"login"];
+            appDelegate.window.rootViewController = loginViewController;
+            
+            NSLog(@"User logged out successfully");
+        } else {
+            NSLog(@"Error logging out: %@", error);
+        }
+    }];
 }
 
 /*
