7b28ee25b6e1c625914e399dfb47932ffcb171a2
Daniela Guajardo <danyguajiba@fb.com>
2019-07-18 14:18:41 -0700

    the user can post, with caption, see the preview of everything and tap share and send it to the database succesfully

--- Fitness App/View Controllers/postViewController.m
+++ Fitness App/View Controllers/postViewController.m
@@ -7,7 +7,9 @@
 //
 
 #import "postViewController.h"
+#import "ProfileViewController.h"
 #import "post.h"
+#import "AppDelegate.h"
 
 @interface postViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
@@ -25,7 +27,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
     PFUser *currentUser = [PFUser currentUser];
     
     self.captionTextView.delegate = self;
@@ -33,13 +34,7 @@
     
     self.captionTextView.text = self.placeholderText;
     self.captionTextView.textColor = [UIColor lightGrayColor];
-    
-//    self.profilePictureImage.file = currentUser[@"profilePicture"];
 }
-
-//- (IBAction)tapClose:(UIBarButtonItem *)sender {
-//    [self dismissViewControllerAnimated:true completion:nil];
-//}
 
 - (IBAction)didTapPicture:(id)sender {
     [self choosePicture:@"Choose an image using your camera or photo library" withTitle:@"Upload a Picture"];
@@ -78,22 +73,11 @@
     }];
 }
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
-    self.originalImage = info[UIImagePickerControllerOriginalImage];
-    self.editedImage = info[UIImagePickerControllerEditedImage];
-    self.editedImage = [self resizeImage:self.originalImage withSize:CGSizeMake(400, 400)];
-
-    self.photo = self.editedImage;
-    NSData *imageData = UIImagePNGRepresentation(self.photo);
-    PFFileObject *file = [PFFileObject fileObjectWithName:@"image.png" data:imageData];
-    PFUser *currentUser = [PFUser currentUser];
-    currentUser[@"post"] = file;
-    [currentUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
-        if (succeeded) {
-            NSLog(@"Successfully uploaded new picture to backend");
-        } else {
-            NSLog(@"%@", error.localizedDescription);
-        }
-    }];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    
+    // Do something with the images (based on your use case)
+    self.photo = editedImage;
+    self.imageView.image = self.photo;
     
     // Dismiss UIImagePickerController to go back to your original view controller
     [self dismissViewControllerAnimated:YES completion:nil];
@@ -141,16 +125,12 @@
         }
         [self dismissViewControllerAnimated:true completion:nil];
     }];
-}
+    
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    ProfileViewController *profileViewController = [storyboard instantiateViewControllerWithIdentifier:@"Profile"];
+    appDelegate.window.rootViewController = profileViewController;
 
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
+}
 
 @end
